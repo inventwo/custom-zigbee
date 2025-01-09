69ba035f870f33d765964135f3e5bff4f88553a9
skvarel <skvarel@inventwo.com>
2025-01-09 23:18:32 +0100

    Add files via upload

---
+++ Converter/CC2531.DS18B20.js
@@ -0,0 +1,59 @@
+const zigbeeHerdsmanConverters = require('zigbee-herdsman-converters');
+const zigbeeHerdsmanUtils = require('zigbee-herdsman-converters/lib/utils');
+
+
+const exposes = zigbeeHerdsmanConverters['exposes'] || require("zigbee-herdsman-converters/lib/exposes");
+const ea = exposes.access;
+const e = exposes.presets;
+const fz = zigbeeHerdsmanConverters.fromZigbeeConverters || zigbeeHerdsmanConverters.fromZigbee;
+const tz = zigbeeHerdsmanConverters.toZigbeeConverters || zigbeeHerdsmanConverters.toZigbee;
+
+const ptvo_switch = (zigbeeHerdsmanConverters.findByModel)?zigbeeHerdsmanConverters.findByModel('ptvo.switch'):zigbeeHerdsmanConverters.findByDevice({modelID: 'ptvo.switch'});
+fz.legacy = ptvo_switch.meta.tuyaThermostatPreset;
+fz.ptvo_on_off = {
+  cluster: 'genOnOff',
+  type: ['attributeReport', 'readResponse'],
+  convert: (model, msg, publish, options, meta) => {
+      if (msg.data.hasOwnProperty('onOff')) {
+          const channel = msg.endpoint.ID;
+          const endpointName = `l${channel}`;
+          const binaryEndpoint = model.meta && model.meta.binaryEndpoints && model.meta.binaryEndpoints[endpointName];
+          const prefix = (binaryEndpoint) ? model.meta.binaryEndpoints[endpointName] : 'state';
+          const property = `${prefix}_${endpointName}`;
+	  if (binaryEndpoint) {
+            return {[property]: msg.data['onOff'] === 1};
+          }
+          return {[property]: msg.data['onOff'] === 1 ? 'ON' : 'OFF'};
+      }
+  },
+};
+
+
+
+const device = {
+    zigbeeModel: ['CC2531.DS18B20'],
+    model: 'CC2531.DS18B20',
+    vendor: 'inventwo',
+    description: '[Configurable firmware](https://ptvo.info/zigbee-configurable-firmware-features/)',
+    fromZigbee: [fz.ignore_basic_report, fz.ptvo_switch_analog_input,],
+    toZigbee: [tz.ptvo_switch_trigger,],
+    exposes: [e.temperature().withEndpoint('l1'),
+      e.temperature().withEndpoint('l2'),
+],
+    meta: {
+        multiEndpoint: true,
+        
+    },
+    endpoint: (device) => {
+        return {
+            l1: 1, l2: 2,
+        };
+    },
+    configure: async (device, coordinatorEndpoint, logger) => {
+      const endpoint = device.getEndpoint(1);
+      await endpoint.read('genBasic', ['modelId', 'swBuildId', 'powerSource']);
+    },
+
+};
+
+module.exports = device;
